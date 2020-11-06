9765afb7cd1624444ed9d39cbca4dbd58922aab0
MarcoFratta <73824299+MarcoFratta@users.noreply.github.com>
2020-11-06 10:07:05 +0100

    final

--- probabilita.c
+++ probabilita.c
@@ -31,16 +31,17 @@ int main() {
 
 	const int N_PANINI = 832;
 
-	int fTotali;
-	int fTrovate;
-	int fPacchetto;
-	int fDaTrovare;
-	int fMancanti;
+	int fTotali = 0;
+	int fTrovate = 0;
+	int fPacchetto = 0;
+	int fDaTrovare = 0;
+	int fMancanti = 0;
 	double prob = 0.0;
 
 	printf(" -- NON INSERIRE ALTRO SE NON NUMERI INTERI -- \n\n");
 	
-	inserisci("Figurine totali: " , MAX_FIGURINE , &fTotali);
+	
+    	inserisci("Figurine totali: " , MAX_FIGURINE , &fTotali);
 	inserisci("Figurine trovate: ", fTotali, &fTrovate);
 	inserisci("Figurine per pacchetto: ", fTotali , &fPacchetto);
 	inserisci("Figurine da trovare: ", fPacchetto, &fDaTrovare);
@@ -50,16 +51,17 @@ int main() {
 	unsigned long long int den = su(fTotali , fPacchetto);
 	
 	// non mi credevate scettici :(
-	for(int i = 0 ; i < fPacchetto - fDaTrovare + 1 ; i++){
-			
-		if(fPacchetto == numeroNuoveEstratte){
-			prob += su(fMancanti,fPacchetto) / den;
-		}
+	for(int i = 0 ; i < fPacchetto - fDaTrovare + 1 ; i++){		
+		if(fPacchetto == numeroNuoveEstratte)
+			prob +=  (double) su(fMancanti,fPacchetto) / (double) den;
+		
 		else{
-			prob += su(fMancanti , numeroNuoveEstratte) * su(fTrovate , fPacchetto - numeroNuoveEstratte);
-			prob /= den;
-		}		
-	}
+		    unsigned long long int a = su(fMancanti , (numeroNuoveEstratte));
+		    unsigned long long int b = su(fTrovate , fPacchetto - (numeroNuoveEstratte));
+		    unsigned long long int num =  a * b; 
+		    prob += (double) num / (double) den;
+       		}     
+    }
 
-	printf("Probabilita' di trovare almeno una carta nuova: %.2f%%", prob);
+	printf("Probabilita' di trovare almeno %d %s da trovare: %.2f%%", fDaTrovare,fDaTrovare == 1 ? "carta" : "carte" , (prob * 100));
 }
